19ed2ec9714d3e0a797456ac0ea65680c9c25963
NISYS\NiLay <nsotnikov@gmail.com>
2017-02-21 18:59:49 +0100

    Add TODO, README changes.

--- README.md
+++ README.md
@@ -3,12 +3,16 @@
 **This shell script will stop and delete all docker containers, images and volumes on your host!**    
 **It will not be possible to restore them!**  
 
-## Usage
-Just execute following command
+<p align="center">
+<img alt="Docker cleanup shell script" src=".github/terminal-scsh.png" width=100%">
+</p>
+
+## Usage 
+Just execute following command in your Terminal (review [the script](https://raw.githubusercontent.com/nsotnikov/cleanup-docker/master/cleanup-docker.sh)):
 ```sh
-$ sh <(curl -sL https://git.io/vDSx7)
+sh <(curl -sL https://git.io/vDSx7)
 ```
-These shell script do:
+This shell script will do:
   - stop and delete all docker containter
   - delete all images 
   - remove all docker volumes 
--- cleanup-docker.sh
+++ cleanup-docker.sh
@@ -8,7 +8,6 @@ printf "Docker Cleanup Shell Script \n\n"
 printf "WARNING! WARNING!! WARNING!!!\n"
 printf "These script will STOP and REMOVE ALL docker containers, images and volumes!\n\n"
 
-# wrapped up in a function to protect half the file during "curl | sh"
 do_clear() {
 while true; do
     read -p "Do you want to continue cleaning? (yes/no): " yn
@@ -24,11 +23,16 @@ while true; do
     esac
  done
 }
+# wrapped up in a function to protect half the file during "curl | sh"
 do_clear
 
 
 
-# TODO 
+
+
+
+
+########### TODO 
 # echo "Removing exited docker containers..."
 # docker ps -a -f status=exited -q | xargs -r docker rm -v
 # echo "Removing dangling images..."
@@ -37,3 +41,46 @@ do_clear
 # docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
 # echo "Removing dangling volumes..."
 # docker volume ls -qf dangling=true | xargs -r docker volume rm
+
+
+################# TODO
+#
+# while :
+# do
+# 	clear
+#         # display menu
+#         echo "Server Name - $(hostname)"
+# 	echo "-------------------------------"
+# 	echo "     M A I N - M E N U"
+# 	echo "-------------------------------"
+# 	echo "1. Display date and time."
+# 	echo "2. Display what users are doing."
+# 	echo "3. Display network connections."
+# 	echo "4. Exit"
+#         # get input from the user 
+# 	read -p "Enter your choice [ 1 -4 ] " choice
+#         # make decision using case..in..esac 
+# 	case $choice in
+# 		1)
+# 			echo "Today is $(date)"
+# 			read -p "Press [Enter] key to continue..." readEnterKey
+# 			;;
+# 		2) 
+# 			w	
+# 			read -p "Press [Enter] key to continue..." readEnterKey
+# 			;;
+# 		3)
+# 			netstat -nat
+# 			read -p "Press [Enter] key to continue..." readEnterKey
+# 			;;
+# 		4)
+# 			echo "Bye!"
+# 			exit 0
+# 			;;
+# 		*)
+# 			echo "Error: Invalid option..."	
+# 			read -p "Press [Enter] key to continue..." readEnterKey
+# 			;;
+# 	esac		
+				
+# done
